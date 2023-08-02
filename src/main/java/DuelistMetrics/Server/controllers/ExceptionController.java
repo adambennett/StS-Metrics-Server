64af82c0428bedf670649b9e56188074d977b2e2
Adam Bennett <nyoxidetwitter@gmail.com>
2023-08-02 01:50:37 -0400

    logged exceptions

---
+++ src/main/java/DuelistMetrics/Server/controllers/ExceptionController.java
@@ -0,0 +1,85 @@
+package DuelistMetrics.Server.controllers;
+
+import DuelistMetrics.Server.models.LoggedException;
+import DuelistMetrics.Server.models.dto.LoggedExceptionDTO;
+import DuelistMetrics.Server.services.ExceptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+public class ExceptionController {
+
+    private static final Logger logger = Logger.getLogger("DuelistMetrics.ExceptionController");
+
+    private final ExceptionService exceptionService;
+
+    @Autowired
+    public ExceptionController(ExceptionService expService) {
+        exceptionService = expService;
+    }
+
+    @PostMapping({"/searchLogsByMessage", "/searchLogsByMessage/{days}"})
+    @CrossOrigin(origins = {"https://sts-metrics-site.herokuapp.com", "http://localhost:4200"})
+    public List<LoggedExceptionDTO> findLogsByMessage(@RequestBody String message, @PathVariable(required = false) String days) {
+        if (days != null) {
+            try {
+                Integer daysParsed = Integer.parseInt(days);
+                return this.exceptionService.searchLogsByMessageDays(message, daysParsed);
+            } catch (Exception ex) {
+                return this.exceptionService.searchLogsByMessage(message);
+            }
+        }
+        return this.exceptionService.searchLogsByMessage(message);
+    }
+
+    @PostMapping({"/searchLogsByTrace", "/searchLogsByTrace/{days}"})
+    @CrossOrigin(origins = {"https://sts-metrics-site.herokuapp.com", "http://localhost:4200"})
+    public List<LoggedExceptionDTO> findLogsByTrace(@RequestBody String trace, @PathVariable(required = false) String days) {
+        if (days != null) {
+            try {
+                Integer daysParsed = Integer.parseInt(days);
+                return this.exceptionService.searchLogsByStackTraceDays(trace, daysParsed);
+            } catch (Exception ex) {
+                return this.exceptionService.searchLogsByStackTrace(trace);
+            }
+        }
+        return this.exceptionService.searchLogsByStackTrace(trace);
+    }
+
+    @GetMapping("/lastXDaysOfLogs/{days}")
+    @CrossOrigin(origins = {"https://sts-metrics-site.herokuapp.com", "http://localhost:4200"})
+    public Object findLogsByTrace(@PathVariable String days) {
+        try {
+            Integer daysParsed = Integer.parseInt(days);
+            return this.exceptionService.findLastXDaysOfLogs(daysParsed);
+        } catch (Exception ex) {
+            return "Exception while fetching logs - " + ex.getMessage();
+        }
+    }
+
+    @PostMapping("/logException")
+    @CrossOrigin(origins = {"https://sts-metrics-site.herokuapp.com", "http://localhost:4200"})
+    public String handleException(@RequestBody LoggedExceptionDTO exception) {
+        this.exceptionService.create(new LoggedException(
+                exception.message(),
+                exception.stackTrace(),
+                exception.uuid(),
+                new Date(),
+                exception.duelistModVersion(),
+                exception.devMessage()
+        ));
+        logger.info("Saved exception - " + exception.message());
+        return "";
+    }
+
+
+}
---
+++ src/main/java/DuelistMetrics/Server/models/LoggedException.java
@@ -0,0 +1,202 @@
+package DuelistMetrics.Server.models;
+
+import DuelistMetrics.Server.models.dto.LoggedExceptionDTO;
+import jakarta.persistence.ColumnResult;
+import jakarta.persistence.ConstructorResult;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedNativeQuery;
+import jakarta.persistence.SqlResultSetMapping;
+
+import java.util.Date;
+
+@Entity
+@NamedNativeQuery(name = "findLastXDaysOfLogsLookup", query = """
+SELECT
+  message,
+  stack_trace AS stackTrace,
+  unique_player_id AS uuid,
+  created_date AS createdDate,
+  duelist_mod_version AS duelistModVersion,
+  dev_message AS devMessage
+FROM logged_exception
+WHERE DATEDIFF(created_date, CURDATE()) > ((:days) * -1)
+""", resultSetMapping = "loggedExceptionDtoXDaysMapping")
+@SqlResultSetMapping(
+        name = "loggedExceptionDtoXDaysMapping",
+        classes = @ConstructorResult(targetClass = LoggedExceptionDTO.class,columns = {
+                @ColumnResult(name = "message", type = String.class),
+                @ColumnResult(name = "stackTrace", type = String.class),
+                @ColumnResult(name = "uuid", type = String.class),
+                @ColumnResult(name = "createdDate", type = Date.class),
+                @ColumnResult(name = "duelistModVersion", type = String.class),
+                @ColumnResult(name = "devMessage", type = String.class)})
+)
+@NamedNativeQuery(name = "findLogsMatchingMessageLookup", query = """
+SELECT
+  message,
+  stack_trace AS stackTrace,
+  unique_player_id AS uuid,
+  created_date AS createdDate,
+  duelist_mod_version AS duelistModVersion,
+  dev_message AS devMessage
+FROM logged_exception
+WHERE message like CONCAT('%', :message, '%')
+""", resultSetMapping = "loggedExceptionDtoMessageMapping")
+@SqlResultSetMapping(
+        name = "loggedExceptionDtoMessageMapping",
+        classes = @ConstructorResult(targetClass = LoggedExceptionDTO.class,columns = {
+                @ColumnResult(name = "message", type = String.class),
+                @ColumnResult(name = "stackTrace", type = String.class),
+                @ColumnResult(name = "uuid", type = String.class),
+                @ColumnResult(name = "createdDate", type = Date.class),
+                @ColumnResult(name = "duelistModVersion", type = String.class),
+                @ColumnResult(name = "devMessage", type = String.class)})
+)
+@NamedNativeQuery(name = "findLogsMatchingMessageByDaysLookup", query = """
+SELECT
+  message,
+  stack_trace AS stackTrace,
+  unique_player_id AS uuid,
+  created_date AS createdDate,
+  duelist_mod_version AS duelistModVersion,
+  dev_message AS devMessage
+FROM logged_exception
+WHERE message like CONCAT('%', :message, '%') AND
+      DATEDIFF(created_date, CURDATE()) > ((:days) * -1)
+""", resultSetMapping = "loggedExceptionDtoMessageByDaysMapping")
+@SqlResultSetMapping(
+        name = "loggedExceptionDtoMessageByDaysMapping",
+        classes = @ConstructorResult(targetClass = LoggedExceptionDTO.class,columns = {
+                @ColumnResult(name = "message", type = String.class),
+                @ColumnResult(name = "stackTrace", type = String.class),
+                @ColumnResult(name = "uuid", type = String.class),
+                @ColumnResult(name = "createdDate", type = Date.class),
+                @ColumnResult(name = "duelistModVersion", type = String.class),
+                @ColumnResult(name = "devMessage", type = String.class)})
+)
+@NamedNativeQuery(name = "findLogsMatchingStackTraceLookup", query = """
+SELECT
+  message,
+  stack_trace AS stackTrace,
+  unique_player_id AS uuid,
+  created_date AS createdDate,
+  duelist_mod_version AS duelistModVersion,
+  dev_message AS devMessage
+FROM logged_exception
+WHERE stack_trace like CONCAT('%', :message, '%')
+""", resultSetMapping = "loggedExceptionDtoTraceMapping")
+@SqlResultSetMapping(
+        name = "loggedExceptionDtoTraceMapping",
+        classes = @ConstructorResult(targetClass = LoggedExceptionDTO.class,columns = {
+                @ColumnResult(name = "message", type = String.class),
+                @ColumnResult(name = "stackTrace", type = String.class),
+                @ColumnResult(name = "uuid", type = String.class),
+                @ColumnResult(name = "createdDate", type = Date.class),
+                @ColumnResult(name = "duelistModVersion", type = String.class),
+                @ColumnResult(name = "devMessage", type = String.class)})
+)
+@NamedNativeQuery(name = "findLogsMatchingStackTraceByDaysLookup", query = """
+SELECT
+  message,
+  stack_trace AS stackTrace,
+  unique_player_id AS uuid,
+  created_date AS createdDate,
+  duelist_mod_version AS duelistModVersion,
+  dev_message AS devMessage
+FROM logged_exception
+WHERE stack_trace like CONCAT('%', :message, '%') AND
+      DATEDIFF(created_date, CURDATE()) > ((:days) * -1)
+""", resultSetMapping = "loggedExceptionDtoTraceByDaysMapping")
+@SqlResultSetMapping(
+        name = "loggedExceptionDtoTraceByDaysMapping",
+        classes = @ConstructorResult(targetClass = LoggedExceptionDTO.class,columns = {
+                @ColumnResult(name = "message", type = String.class),
+                @ColumnResult(name = "stackTrace", type = String.class),
+                @ColumnResult(name = "uuid", type = String.class),
+                @ColumnResult(name = "createdDate", type = Date.class),
+                @ColumnResult(name = "duelistModVersion", type = String.class),
+                @ColumnResult(name = "devMessage", type = String.class)})
+)
+public class LoggedException {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String devMessage;
+  private String message;
+  private String stackTrace;
+  private String uniquePlayerId;
+  private String duelistModVersion;
+  private Date createdDate;
+
+  public LoggedException() {}
+
+  public LoggedException(String message, String stackTrace, String uniquePlayerId, Date createdDate, String duelistModVersion, String devMessage) {
+    this.message = message;
+    this.stackTrace = stackTrace;
+    this.uniquePlayerId = uniquePlayerId;
+    this.createdDate = createdDate;
+    this.duelistModVersion = duelistModVersion;
+    this.devMessage = devMessage;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getStackTrace() {
+    return stackTrace;
+  }
+
+  public void setStackTrace(String stackTrace) {
+    this.stackTrace = stackTrace;
+  }
+
+  public String getUniquePlayerId() {
+    return uniquePlayerId;
+  }
+
+  public void setUniquePlayerId(String uuid) {
+    this.uniquePlayerId = uuid;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public String getDuelistModVersion() {
+    return duelistModVersion;
+  }
+
+  public void setDuelistModVersion(String duelistModVersion) {
+    this.duelistModVersion = duelistModVersion;
+  }
+
+  public String getDevMessage() {
+    return devMessage;
+  }
+
+  public void setDevMessage(String devMessage) {
+    this.devMessage = devMessage;
+  }
+}
