a0a2ef51da4518848f9402c821ab33ea29bd8b1b
Adam Bennett <nyoxidetwitter@gmail.com>
2023-08-02 03:46:04 -0400

    add uuid to bundle

--- src/main/java/DuelistMetrics/Server/models/Bundle.java
+++ src/main/java/DuelistMetrics/Server/models/Bundle.java
@@ -35,6 +35,7 @@ public class Bundle {
   private String starting_deck;
   private String country;
   private String lang;
+  private String unique_player_id;
 
   private Boolean add_base_game_cards;
   private Boolean allow_boosters;
@@ -925,6 +926,14 @@ public class Bundle {
     this.challenge_level = challenge_level;
   }
 
+  public String getUnique_player_id() {
+    return unique_player_id;
+  }
+
+  public void setUnique_player_id(String unique_player_id) {
+    this.unique_player_id = unique_player_id;
+  }
+
   @Override
   public String toString() {
     return "Bundle{" +
@@ -1008,6 +1017,7 @@ public class Bundle {
             ", relics_obtained=" + relics_obtained +
             ", campfire_choices=" + campfire_choices +
             ", damage_taken=" + damage_taken +
+            ", unique_player_id=" + unique_player_id +
             '}';
   }
 }
